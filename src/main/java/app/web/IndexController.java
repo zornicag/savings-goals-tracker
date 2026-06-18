680df6b1a4f252f8d11628e7ff45464b20ea2980
zorni <zorni@DESKTOP-GPHNEER.(none)>
2026-06-19 01:09:05 +0300

    feat: add landing and dashboard pages

--- src/main/java/app/web/IndexController.java
+++ src/main/java/app/web/IndexController.java
@@ -2,12 +2,18 @@ package app.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/home")
+    public ModelAndView home() {
+        return new ModelAndView("home");
     }
 }
